0670bcf63dc71fe70df7166b3e44f2cd1c821c8f
ballanse <devblackholes@gmail.com>
2011-12-19 23:56:34 -0600

    moving to twitter bootstrap

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -2,17 +2,13 @@
 <html>
 <head>
   <title>The Laravel Playground</title>
-  <link rel="stylesheet" type="text/css" href="<?php echo URL::to_asset('css/style.css'); ?>" />
+  <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">
   <?php echo Asset::styles(); ?>
 </head>
 <body>
   <div class="container">
     <h1>Laravel Powered Blog</h1>
     <div id="nav">
-      <li class="separator">|</li>
-      <?php if ( ! Auth::check()): ?>
-      <li class="separator">|</li>
-      <?php endif; ?>
     </div>
     <?php echo $content; ?>
   </div>
