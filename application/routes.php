069d78259b1e8aa17d3964cb94890923ac26b4f3
ballanse <devblackholes@gmail.com>
2011-12-19 20:33:48 -0600

    forgot paren in get /posts

--- application/routes.php
+++ application/routes.php
@@ -11,7 +11,7 @@ return array(
 		));
 	},
 
-	'GET /posts/(\d+)' => array('name' => 'show_post', function($id)
+	'GET /posts/(\d+)' => array('name' => 'show_post', function($id))
 	{
 	  $post = Post::find($id);
 	  return View::make('layouts.default')->nest('content', 'posts.show', array(
