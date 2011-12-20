58b7418791da0377c35ac8a4538f2401c6a6a721
ballanse <devblackholes@gmail.com>
2011-12-19 20:05:57 -0600

    Update application/views/layouts/default.php

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -6,17 +6,12 @@
   <?php echo Asset::styles(); ?>
 </head>
 <body>
-  <ul>
-    <li><?php echo HTML::link_to_new_posts('Add Post'); ?></li>\
-  </ul>
   <div class="container">
     <h1>Laravel Powered Blog</h1>
     <div id="nav">
-      <li><?php echo HTML::link_to_posts('Posts'); ?></li>
       <li class="separator">|</li>
       <?php if ( ! Auth::check()): ?>
       <li class="separator">|</li>
-      <li><?php echo HTML::link_to_login('Login'); ?></li>
       <?php endif; ?>
     </div>
     <?php echo App::flash_notice(); ?>
