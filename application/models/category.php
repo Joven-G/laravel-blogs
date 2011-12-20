67f6b6264fcc1c8707b9d7654c4171a43a6e54e6
ballanse <devblackholes@gmail.com>
2011-12-19 20:24:54 -0600

    adding category model

---
+++ application/models/category.php
@@ -0,0 +1,37 @@
+<?php
+
+class Category extend Eloquent
+{
+
+  public static $timestamps = true;
+
+  private $errors = array();
+
+  public function posts()
+  {
+    return $this->has_many('Post');
+  }
+
+  public function errors()
+  {
+    return $this->errors;
+  }
+
+  public function is_valid()
+  {
+    $validator = Validator::make($this->attributes, array(
+      'name' => array('required', 'unique:categories'),
+    ));
+    if ($validator->valid())
+    {
+      $this->errors = array();
+      return true;
+    }
+    else
+    {
+      $this->errors = $validator->errors->all();
+      return false;
+    }
+  }
+
+}
