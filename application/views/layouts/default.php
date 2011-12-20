aed9855832fe8dc099e29e2eac68d70b71bbee77
ballanse <devblackholes@gmail.com>
2011-12-20 00:34:10 -0600

    changed css load order

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -2,8 +2,8 @@
 <html>
 <head>
   <title>The Laravel Playground</title>
-  <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="<?php echo URL::to_asset('css/style.css'); ?>" />
+  <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">
   <?php echo Asset::styles(); ?>
 </head>
 <body>
