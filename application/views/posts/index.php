c4abbc872661e37198de300b50dd0334755fb218
ballanse <devblackholes@gmail.com>
2011-12-20 18:23:09 -0600

    class cleanup in post/index.php

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -3,15 +3,11 @@
 </div>
 
 <?php foreach ($posts->results as $post): ?>
-<div class="post_box">
-  <div class="page-header">
-  <h3><?php echo HTML::entities($post->title); ?></h3>
-  <div class="author">By: <strong><?php echo $post->user->username; ?></strong></div>
-  <div class="category">
-    Published under: <strong><?php echo $post->category->name; ?></strong>
-  </div>
-  <br />
-  <div class="content"><?php echo HTML::entities($post->content); ?></div>
+<h3><?php echo HTML::entities($post->title); ?></h3>
+<div class="author">By: <strong><?php echo $post->user->username; ?></strong></div>
+<div class="category">Published under: <strong><?php echo $post->category->name; ?></strong></div>
+<br />
+<div class="content"><?php echo HTML::entities($post->content); ?></div>
   <?php echo HTML::link(URL::to_show_post(array($post->id)), 'More'); ?>
 </div>
 <?php endforeach; ?>
