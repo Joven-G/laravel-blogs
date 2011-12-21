d7fd77f0d2e77930ff0a763796352f497213fe69
ballanse <devblackholes@gmail.com>
2011-12-21 10:02:09 -0600

    Update application/views/posts/index.php

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,14 +1,13 @@
 <div class="page-header">
   <h1>Posts <small>Viewing all posts</small></h1>
-</div>
+</div><!-- end of page-header div -->
 
 <?php foreach ($posts->results as $post): ?>
-<h3><?php echo HTML::entities($post->title); ?></h3>
-<div class="author">By: <strong><?php echo $post->user->username; ?></strong></div>
-<div class="category">Published under: <strong><?php echo $post->category->name; ?></strong></div>
-<br />
-<div class="content"><?php echo HTML::entities($post->content); ?></div>
-  <?php echo HTML::link(URL::to_show_post(array($post->id)), 'More'); ?>
-</div>
-<?php endforeach; ?>
-<?php echo $posts->links(); ?>
+      <h3><?php echo HTML::entities($post->title); ?></h3>
+      <div class="author">By: <strong><?php echo $post->user->username; ?></strong></div>
+      <div class="category">Published under: <strong><?php echo $post->category->name; ?></strong></div>
+      <br />
+      <?php echo HTML::entities($post->content); ?>
+      <?php echo HTML::link(URL::to_show_post(array($post->id)), 'More'); ?>
+      <?php endforeach; ?>
+      <?php echo $posts->links(); ?>
